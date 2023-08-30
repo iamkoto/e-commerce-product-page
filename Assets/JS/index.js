6e383f49df5a5081c45cd0a13c6feb1c1f78d505
iamkoto <134742316+iamkoto@users.noreply.github.com>
2023-08-30 16:07:10 -0300

    slick error

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -1,3 +1,17 @@
 $(Product-page).ready(function(){
-    
+    $('.spot-left__bigimg').slick({
+        slidesToShow: 1,
+        slitesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavfor: '.spot-left__smallimg',    
+    });
+    $('.spot-left__smallimg').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavfor: '.spot-left__bigimg',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true,
+    });
 })
