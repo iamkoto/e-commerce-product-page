1bbe721fe01196e514b581c26d1889988b299dd7
iamkoto <134742316+iamkoto@users.noreply.github.com>
2023-09-08 18:26:20 -0300

    Mudanças no jquary

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
         swipeToSlide: false,
         asNavFor: '.spot-left__smallimgs',    
     });
+
     $('.spot-left__smallimgs').slick({
         slidesToShow: 4,
         slidesToScroll: 1,
@@ -16,6 +17,7 @@ $(document).ready(function(){
         infinite: true,
         focusOnSelect: true,
     });
+
     $('.spot-right__buy-plus').on('click', function(){
         var inputField = $('#spot-right__buy-quantity');
         var currentValue = parseInt(inputField.val());
@@ -27,6 +29,7 @@ $(document).ready(function(){
             alert('Compra máxima de 15 produtos')
         }
       });
+
       $('.spot-right__buy-minus').on('click', function(){
         var inputField = $('#spot-right__buy-quantity');
         var currentValue = parseInt(inputField.val());
@@ -36,21 +39,50 @@ $(document).ready(function(){
             inputField.val(currentValue);
         }
       });
-      $('#buy').on('click', function(){
+
+      function addToCart() {
         var quant = $('#quant');
-        var price = $('#price');
+        var price = $('#price').text();
         var priceQuant = parseInt(price);
         var priceEnd = $('#price-end');
         // Pega a quantidade de produtos
         var inputField = $('#spot-right__buy-quantity');
         var currentValue = parseInt(inputField.val());
+        var priceCalc = currentValue * priceQuant;
         quant.text(currentValue);
         // Fim pega a quantidade de produtos
         // Calcula o preço final
-        console.log(2 * currentValue)
+        priceEnd.text(priceCalc + '.00');
         // Fim calcula o preço final
+        $('.header-right__cart-box__empty').removeClass('active');
+        $('.header-right__cart-box__full').toggleClass('active');
+      };
 
+      function deleteProd() {
+        $('#delete').on('click', function(){
+          $(this).parent().remove();
+          $('.header-right__cart-box__empty').toggleClass('active');
+        });
+      };
+
+      function createProd() {
+        var fullBox = $('.header-right__cart-box__full');
+        
+        fullBox.prepend(`
+          <div class="header-right__cart-box__product">
+            <img class="header-right__cart-box__img" src="./Assets/images/image-product-1-thumbnail.jpg" alt="product1">
+            <p>Fall Limited Edition Sneakers $<span id="price">125.00</span> x <span id="quant">1</span> $<span id="price-end">125.00</span></p>
+            <img id="delete" onclick="deleteProd()" src="./Assets/images/icon-delete.svg" alt="delete">
+          </div>
+        `);
+      } 
+
+      $('#buy').on('click', function(){
+        createProd();
+        addToCart();
+        deleteProd();
       });
+
       $('.header-right__cart-img').on('click', function(){
         $('.header-right__cart-box').toggleClass('active')
       });
